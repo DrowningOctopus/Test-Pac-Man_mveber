ab0b88dabfc745ab32eb478671a924e539f7c1eb
DrowningOctopus <17785019+DrowningOctopus@users.noreply.github.com>
2020-09-23 14:58:40 -0400

    Add Pac-Man and make him move
    Created a GameObject personifying Pac-Man and the script enabling it to move following user inputs

---
+++ Test Pac-Man_mveber/Assets/Scripts/PacManController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PacManController : MonoBehaviour
+{
+    public float Speed = 2f;
+    CharacterController PacManCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // PacMan is initialized as the controllable character
+        PacManCharacter = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Retrieving the user inputs
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+
+        // Moving PacMan according to user inputs
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+        PacManCharacter.Move(move * Time.deltaTime * Speed);
+    }
+}
